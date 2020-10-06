0b31bce710d1828a822ba746c0068994b183db44
mr-zero <amirajubolchi001@gmail.com>
2020-10-06 20:12:42 +0530

    modified for added plugs

--- OkiDoki/OkiDoki/settings.py
+++ OkiDoki/OkiDoki/settings.py
@@ -20,6 +20,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+# Added Plugin
+INSTALLED_APPS += [
+    'django_extensions',
+]
+
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -74,6 +80,19 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+if DEBUG:
+    INSTALLED_APPS += [
+        'debug_toolbar',
+    ]
+    MIDDLEWARE += [
+        'debug_toolbar.middleware.DebugToolbarMiddleware',
+    ]
+    INTERNAL_IPS = [
+        '127.0.0.1',
+        'localhost',
+    ]
+
+
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -85,3 +104,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticdir'),)
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
