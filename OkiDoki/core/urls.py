e43d87c849b37136fb8f979ac67c02220c142a1c
mr-zero <amirajubolchi001@gmail.com>
2020-10-07 04:28:51 +0530

    add auths in core - login, signup & logout

--- OkiDoki/core/urls.py
+++ OkiDoki/core/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
-from core.views import frontpage
+from core.views import frontpage, signup, logout_user, login_user
 
 app_name = "core"
 
 urlpatterns = [
     path('', frontpage, name="frontpage"),
+    path('signup', signup, name="signup"),
+    path('logout', logout_user, name="logout"),
+    path('login', login_user, name="login"),
 ]
--- OkiDoki/core/views.py
+++ OkiDoki/core/views.py
@@ -1,5 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout, authenticate
+from core.forms import UserLoginForm
 
 
 def frontpage(request):
     return render(request, 'core/frontpage.html', {})
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST or None)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('core:frontpage')
+        else:
+            return render(request, 'core/signup.html', {'form': form})
+    form = UserCreationForm()
+    return render(request, 'core/signup.html', {'form': form})
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('core:frontpage')
+
+
+def login_user(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST or None)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('core:frontpage')
+        else:
+            return render(request, 'core/login.html', {'form': form})
+    form = UserLoginForm()
+    return render(request, 'core/login.html', {'form': form})
