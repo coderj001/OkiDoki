e6abc12f7910d10c0fadf2f07be6085011a6f27e
mr-zero <amirajubolchi001@gmail.com>
2020-10-10 17:48:35 +0530

    django added humanize time

--- OkiDoki/OkiDoki/settings.py
+++ OkiDoki/OkiDoki/settings.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.humanize',
     'django.contrib.staticfiles',
 
     'core',
